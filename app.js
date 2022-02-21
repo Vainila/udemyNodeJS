0ef44f23a3f451b402b7ac4b90ca81bcbdcfef07
vainila <workmail1421@gmail.com>
2022-02-21 15:15:38 +0300

    19

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => {
-   res.send("<h1>Some text inside h1</h1>");
-});
-
 app.listen(3000);
+
+app.get("/", (req, res) => {
+   res.send("Homepage");
+});
+app.get("/add-item", (req, res) => {
+   res.send("<h1> Add items</h1>");
+});
