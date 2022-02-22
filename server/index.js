bf3b25a015b39b2ce4ccca97297d3cedcac4196d
vainila <workmail1421@gmail.com>
2022-02-22 10:10:01 +0300

    upd

---
+++ server/index.js
@@ -0,0 +1,25 @@
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import cors from "cors";
+
+const app = express();
+dotenv.config();
+app.use(express.json({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+const mongoDB =
+   "mongodb+srv://v:vainila@cluster0.h2anz.mongodb.net/mernDB?retryWrites=true&w=majority";
+
+app.get("/", (req, res) => {
+   res.send("Hello World");
+});
+
+const PORT = process.env.PORT || 5000;
+mongoose
+   .connect(mongoDB, { useUnifiedTopology: true, useNewUrlParser: true })
+   .then(() => {
+      console.log(`server is running on port ${PORT}`);
+   })
+   .catch((err) => console.log(err));
