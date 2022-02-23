cae761b51203fd4fc492e93d79e773d7d52e656d
vainila <workmail1421@gmail.com>
2022-02-23 14:20:48 +0300

    add: messages

--- client/src/components/chat/Chat.js
+++ client/src/components/chat/Chat.js
@@ -2,18 +2,25 @@ import React, { useContext, useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
 import { UserContext } from "../../UserContext";
 import io from "socket.io-client";
+
 let socket;
 
 const Chat = () => {
    let { room_id, room_name } = useParams();
    const { user, setUser } = useContext(UserContext);
    const [message, setMessage] = useState("");
+   const [messages, setMessages] = useState([]);
 
    const ENDPT = "localhost:5000";
    useEffect(() => {
       socket = io(ENDPT);
       socket.emit("join", { name: user.name, room_id, user_id: user.id });
    }, []);
+   useEffect(() => {
+      socket.on("message", (message) => {
+         setMessages([...messages, message]);
+      });
+   }, [messages]);
 
    const sendMessage = (event) => {
       event.preventDefault();
@@ -27,6 +34,9 @@ const Chat = () => {
       <div>
          {room_id}
          {room_name}
+
+         <pre>{JSON.stringify(messages, null, 2)}</pre>
+
          <div>Chat {JSON.stringify(user)}</div>
          <form onSubmit={sendMessage}>
             <input
